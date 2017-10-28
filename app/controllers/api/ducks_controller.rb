3f9be5ab152ca687b564e8e4587b8e12f46fb68c
shihokambara <shihokambara@gmail.com>
2017-10-28 16:52:29 +0900

    set routes

--- app/controllers/api/ducks_controller.rb
+++ app/controllers/api/ducks_controller.rb
@@ -7,6 +7,24 @@ module Api
     def create
     end
 
+    def add
+    end
+
+    def list
+    end
+
+    def show
+    end
+
+    def deilylog
+    end
+
+    def update_alertlog
+    end
+
+    def reaction_log
+    end
+
   end
 end
  
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,12 @@ Rails.application.routes.draw do
   post 'api/auth/signup' => "api/authorizations#signup"
   post 'api/auth/signin' => "api/authorizations#signin"
   post 'api/auth/profile/edit' => "api/authorizations#edit"
-  post 'api/duck/create' => 
+  post 'api/duck/create' => "api/ducks#create"
+  post 'api/duck/register' => 'api/ducks#add'
+  post 'api/duck/list' => 'api/ducks#list'
+  post 'api/duck/detail' => 'api/ducks#show'
+  post 'api/duck/dailylog' => 'api/ducks#dailylog'
+  post 'api/duck/alertlog' => 'api/ducks#alertlog'
+  post 'api/duck/alertlog/update' => 'api/ducks#update_alertlog'
+  post 'api/duck/reactionlog' => 'api/ducks#reaction_log'
 end
